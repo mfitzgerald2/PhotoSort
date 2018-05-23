143bf66db7a5ea219d7afc18287893b80f91b68f
Matthew Fitzgerald <31381527+mfitzgerald2@users.noreply.github.com>
2018-05-23 09:54:44 -0500

    Initial Commit
    The initial version of the software

---
+++ PhotoSortV2/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace PhotoSortV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.outputButton = new System.Windows.Forms.Button();
+            this.inputButton = new System.Windows.Forms.Button();
+            this.undoButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.yesButton = new System.Windows.Forms.Button();
+            this.maybeButton = new System.Windows.Forms.Button();
+            this.noButton = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 66.66666F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.04762F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1230, 751);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1224, 494);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(187, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1040, 107);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PhotoSort By Fitzgerald Tech Solutions";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Image = global::PhotoSortV2.Properties.Resources.ftslogo;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(178, 101);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel2.Controls.Add(this.tableLayoutPanel2);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 610);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(178, 138);
+            this.flowLayoutPanel2.TabIndex = 5;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.outputButton, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.inputButton, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.undoButton, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(174, 134);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // outputButton
+            // 
+            this.outputButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputButton.Location = new System.Drawing.Point(3, 102);
+            this.outputButton.Name = "outputButton";
+            this.outputButton.Size = new System.Drawing.Size(168, 29);
+            this.outputButton.TabIndex = 3;
+            this.outputButton.Text = "Output Directory";
+            this.outputButton.UseVisualStyleBackColor = true;
+            this.outputButton.Click += new System.EventHandler(this.outputButton_Click);
+            // 
+            // inputButton
+            // 
+            this.inputButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputButton.Location = new System.Drawing.Point(3, 69);
+            this.inputButton.Name = "inputButton";
+            this.inputButton.Size = new System.Drawing.Size(168, 27);
+            this.inputButton.TabIndex = 2;
+            this.inputButton.Text = "Input Directory";
+            this.inputButton.UseVisualStyleBackColor = true;
+            this.inputButton.Click += new System.EventHandler(this.inputButton_Click);
+            // 
+            // undoButton
+            // 
+            this.undoButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.undoButton.Location = new System.Drawing.Point(3, 36);
+            this.undoButton.Name = "undoButton";
+            this.undoButton.Size = new System.Drawing.Size(168, 27);
+            this.undoButton.TabIndex = 5;
+            this.undoButton.Text = "Undo!";
+            this.undoButton.UseVisualStyleBackColor = true;
+            this.undoButton.Click += new System.EventHandler(this.undoButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Settings:";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.Controls.Add(this.yesButton, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.maybeButton, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.noButton, 2, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(187, 610);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(1040, 138);
+            this.tableLayoutPanel3.TabIndex = 6;
+            // 
+            // yesButton
+            // 
+            this.yesButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.yesButton.Location = new System.Drawing.Point(3, 3);
+            this.yesButton.Name = "yesButton";
+            this.yesButton.Size = new System.Drawing.Size(340, 132);
+            this.yesButton.TabIndex = 0;
+            this.yesButton.Text = "Yes";
+            this.yesButton.UseVisualStyleBackColor = true;
+            this.yesButton.Click += new System.EventHandler(this.yesButton_Click);
+            // 
+            // maybeButton
+            // 
+            this.maybeButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.maybeButton.Location = new System.Drawing.Point(349, 3);
+            this.maybeButton.Name = "maybeButton";
+            this.maybeButton.Size = new System.Drawing.Size(340, 132);
+            this.maybeButton.TabIndex = 1;
+            this.maybeButton.Text = "Maybe";
+            this.maybeButton.UseVisualStyleBackColor = true;
+            this.maybeButton.Click += new System.EventHandler(this.maybeButton_Click);
+            // 
+            // noButton
+            // 
+            this.noButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.noButton.Location = new System.Drawing.Point(695, 3);
+            this.noButton.Name = "noButton";
+            this.noButton.Size = new System.Drawing.Size(342, 132);
+            this.noButton.TabIndex = 2;
+            this.noButton.Text = "No";
+            this.noButton.UseVisualStyleBackColor = true;
+            this.noButton.Click += new System.EventHandler(this.noButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1230, 751);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "PhotoSort by Fitzgerald Tech Solutions";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button inputButton;
+        private System.Windows.Forms.Button outputButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button yesButton;
+        private System.Windows.Forms.Button maybeButton;
+        private System.Windows.Forms.Button noButton;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.Button undoButton;
+    }
+}
+
---
+++ PhotoSortV2/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoSortV2
+{
+    public partial class Form1 : Form
+    {
+
+        private string InPath;
+        private string OutPath;
+        private string FileName;
+        private string LastStored;
+        private string LastStoredFilename; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Open FTS Website
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            //Open FTS WEBSITE
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode.ToString() == "1")
+            {
+                MessageBox.Show("You must select an input directory first!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                yesButton.PerformClick();
+            }
+            if (e.KeyCode.ToString() == "2")
+            {
+                maybeButton.PerformClick();
+            }
+            if (e.KeyCode.ToString() == "3")
+            {
+                noButton.PerformClick();
+            }
+        }
+
+
+        private void inputButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.DirectoryInfo indir = new System.IO.DirectoryInfo(folderBrowserDialog1.SelectedPath);
+
+                System.IO.FileInfo[] Images = indir.GetFiles("*.jpg");
+                string parentheses = "/";
+                // IF ARRAY IS NULL THROW ERROR 
+                if (Images == null)
+                {
+                    MessageBox.Show("You must select a directory with images in it.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    InPath = folderBrowserDialog1.SelectedPath + parentheses;
+                    if (Images.Length < 1)
+                    {
+                        MessageBox.Show("You must select a directory with images in it.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                    else
+                    {
+                        FileName = Convert.ToString(Images[0]);
+                        pictureBox1.Load(InPath + FileName);
+                    }
+                    
+                }
+               
+                
+            }
+        }
+
+        private void outputButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.DirectoryInfo outdir = new System.IO.DirectoryInfo(folderBrowserDialog2.SelectedPath);
+
+                string parentheses = "/";
+                OutPath = outdir + parentheses;
+                
+                if (!(System.IO.Directory.Exists(OutPath + "Yes"))) 
+                {
+                    System.IO.Directory.CreateDirectory(OutPath + "Yes");
+                }
+                if (!(System.IO.Directory.Exists(OutPath + "Maybe")))
+                {
+                    System.IO.Directory.CreateDirectory(OutPath + "Maybe");
+                }
+                if (!(System.IO.Directory.Exists(OutPath + "No")))
+                {
+                    System.IO.Directory.CreateDirectory(OutPath + "No");
+                }
+                MessageBox.Show("All required directories were sucessfully created in the output destination specified.", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void yesButton_Click(object sender, EventArgs e)
+        {
+            string source = InPath + FileName;
+            string dest = OutPath + "yes/" + FileName;
+            pictureBox1.Load(System.IO.Directory.GetCurrentDirectory() + "/Resources/ftslogo.png");
+            if(InPath == null) 
+            {
+                MessageBox.Show("You must select an input directory first!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (OutPath == null)
+            {
+                MessageBox.Show("You must select an output directory first!" , "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                System.IO.File.Move(source, dest);
+                LastStored = dest;
+                LastStoredFilename = FileName;
+                System.IO.DirectoryInfo indir = new System.IO.DirectoryInfo(InPath);
+
+                 System.IO.FileInfo[] Images = indir.GetFiles("*.jpg");
+
+                 if (Images.Length < 1)
+               {
+                  MessageBox.Show("Sorting is Complete!", "Yay!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    
+                 }
+               else
+              {
+                  FileName = Convert.ToString(Images[0]);
+                   pictureBox1.Load(InPath + FileName);
+               }
+            }
+
+        }
+
+        private void maybeButton_Click(object sender, EventArgs e)
+        {
+            string source = InPath + FileName;
+            string dest = OutPath + "maybe/" + FileName;
+            pictureBox1.Load(System.IO.Directory.GetCurrentDirectory() + "/Resources/ftslogo.png");
+            if (InPath == null)
+            {
+                MessageBox.Show("You must select an input directory first!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (OutPath == null)
+            {
+                MessageBox.Show("You must select an output directory first!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                System.IO.File.Move(source, dest);
+                LastStored = dest;
+                LastStoredFilename = FileName;
+
+                System.IO.DirectoryInfo indir = new System.IO.DirectoryInfo(InPath);
+
+                System.IO.FileInfo[] Images = indir.GetFiles("*.jpg");
+
+                if (Images.Length < 1)
+                {
+                    MessageBox.Show("Sorting is Complete!", "Yay!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else
+                {
+                    FileName = Convert.ToString(Images[0]);
+                    pictureBox1.Load(InPath + FileName);
+                }
+            }
+        }
+
+        private void noButton_Click(object sender, EventArgs e)
+        {
+            string source = InPath + FileName;
+            string dest = OutPath + "no/" + FileName;
+            pictureBox1.Load(System.IO.Directory.GetCurrentDirectory() + "/Resources/ftslogo.png");
+            if (InPath == null)
+            {
+                MessageBox.Show("You must select an input directory first!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (OutPath == null)
+            {
+                MessageBox.Show("You must select an output directory first!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                System.IO.File.Move(source, dest);
+                LastStored = dest;
+                LastStoredFilename = FileName;
+
+                System.IO.DirectoryInfo indir = new System.IO.DirectoryInfo(InPath);
+
+                System.IO.FileInfo[] Images = indir.GetFiles("*.jpg");
+
+                if (Images.Length < 1)
+                {
+                    MessageBox.Show("Sorting is Complete!", "Yay!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else
+                {
+                    FileName = Convert.ToString(Images[0]);
+                    pictureBox1.Load(InPath + FileName);
+                }
+            }
+        }
+
+        private void undoButton_Click(object sender, EventArgs e)
+        {
+            if (LastStored == null)
+            {
+                MessageBox.Show("There is nothing to undo! You can't undo more than once!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                System.IO.File.Move(LastStored, InPath + LastStoredFilename);
+                LastStored = null;
+                MessageBox.Show("The image has been restored sucessfully! You can sort it next", "Yay!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 49)
+            {
+                MessageBox.Show("You must select an input directory first!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                yesButton.PerformClick();
+            }
+            if (e.KeyChar == 50)
+            {
+                maybeButton.PerformClick();
+            }
+            if (e.KeyChar == 51)
+            {
+                noButton.PerformClick();
+            }
+        }
+    }
+}
